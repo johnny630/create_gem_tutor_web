34b1dc33ad11685f6893be608125e8cf50e3ea70
johnnyliu <bakolama@gmail.com>
2021-11-04 22:29:52 +0800

    Step19: postgres test `where` and `all` etc.. -> create_tem_tutor Step 24

--- README.md
+++ README.md
@@ -58,3 +58,5 @@ create a `sqlite_test.rb` and run `bundle exec ruby sqlite_test.rb` then can see
 ## Step17: postgres test setting attribute value -> create_tem_tutor Step 22
 
 ## Step18: postgres test `save` and `count` -> create_tem_tutor Step 23
+
+## Step19: postgres test `where` and `all` etc.. -> create_tem_tutor Step 24
--- postgres_test.rb
+++ postgres_test.rb
@@ -8,12 +8,19 @@ end
 
 # puts Task.schema
 
-task = Task.new(title: 'rails test', content: 'content test')
-puts task.title
+# task = Task.new(title: 'rails test', content: 'content test')
+# puts task.title
 
-task.title = 'python test'
-puts task.title
+# task.title = 'python test'
+# puts task.title
 
-puts task.save   # false
-puts Task.count
-puts task.save!  # true
+# puts task.save   # false
+# puts Task.count
+# puts task.save!  # true
+
+# puts Task.all
+# puts Task.all.first.title
+
+Task.where(title: 'rails test').where(content: 'content test').each do |task|
+  puts task.title
+end
